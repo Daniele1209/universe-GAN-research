970b0f38006a22af0648610ff013c719b3a9f7a9
Daniele <dany.mosh@yahoo.com>
2021-11-05 09:45:31 +0200

    first train on padded images (Nebula) -> done

--- generator/GAN.py
+++ generator/GAN.py
@@ -14,31 +14,12 @@ import torchvision.transforms as T
 from torch.utils.data import DataLoader
 from torchvision.utils import make_grid
 from torchvision.utils import save_image
-
-DATA_DIR = ''
-
-stats = (.5, .5, .5), (.5, .5, .5)
-
-transform_ds = T.Compose([
-    T.Resize((128, 128)),
-    T.CenterCrop(128),
-    T.RandomHorizontalFlip(),
-    T.RandomVerticalFlip(),
-    T.ToTensor(),
-    T.Normalize(*stats)
-])
-ds = torchvision.datasets.ImageFolder(root=DATA_DIR, transform=transform_ds)
+import numpy as np
+from multiprocessing import freeze_support
 
 
 def denorm(img_tensor):
     return img_tensor * stats[1][0] + stats[0][0]
-
-
-batch_size = 16
-
-train_dl = DataLoader(ds, batch_size, shuffle=True,
-                      num_workers=3, pin_memory=True)
-
 
 def show_image(train_dl):
     for images, _ in train_dl:
@@ -48,10 +29,6 @@ def show_image(train_dl):
         ax.imshow(
             make_grid(denorm(images.detach()[:32]), nrow=8).permute(1, 2, 0))
         break
-
-
-show_image(train_dl)
-
 
 def get_device():
     if torch.cuda.is_available():
@@ -77,93 +54,6 @@ class DeviceDataLoader():
 
     def __len__(self):
         return len(self.dl)
-
-
-device = get_device()
-
-train_dl = DeviceDataLoader(train_dl, device)
-
-discriminator = nn.Sequential(
-    #in: 128 x 3 x 128 x 128
-
-    nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(64),
-    nn.LeakyReLU(0.2, inplace=True),
-    #128 x 64 x 64 x 64
-
-    nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(128),
-    nn.LeakyReLU(0.2, inplace=True),
-    #128 x 128 x 32 x 32
-
-    nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(256),
-    nn.LeakyReLU(0.2, inplace=True),
-    #128 x 256 x 16 x 16
-
-    nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(512),
-    nn.LeakyReLU(0.2, inplace=True),
-    #128 x 512 x 8 x 8
-
-    nn.Conv2d(512, 1024, kernel_size=4, stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(1024),
-    nn.LeakyReLU(0.2, inplace=True),
-    #128 x 1024 x 4 x 4
-
-    nn.Conv2d(1024, 1, kernel_size=4, stride=1, padding=0, bias=False),
-    #128 x 1 x 1 x 1
-
-    nn.Flatten(),
-    nn.Sigmoid()
-
-)
-
-discriminator = to_device(discriminator, device)
-
-latent_size = 128
-generator = nn.Sequential(
-    #in: 128 x 1 x 1
-
-    nn.ConvTranspose2d(latent_size, 1024, kernel_size=4,
-                       stride=1, padding=0, bias=False),
-    nn.BatchNorm2d(1024),
-    nn.ReLU(True),
-    #128 x 1024 x 4 x 4
-
-    nn.ConvTranspose2d(1024, 512, kernel_size=4,
-                       stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(512),
-    nn.ReLU(True),
-    #128 x 512 x 8 x 8
-
-    nn.ConvTranspose2d(512, 256, kernel_size=4,
-                       stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(256),
-    nn.ReLU(True),
-    #128 x 256 x 16 x 16
-
-    nn.ConvTranspose2d(256, 128, kernel_size=4,
-                       stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(128),
-    nn.ReLU(True),
-    #128 x 128 x 32 x 32
-
-    nn.ConvTranspose2d(128, 64, kernel_size=4,
-                       stride=2, padding=1, bias=False),
-    nn.BatchNorm2d(64),
-    nn.ReLU(True),
-    #128 x 64 x 64 x 64
-
-    nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
-    #128 x 3 x 128 x 128
-    nn.Tanh()
-
-
-)
-
-
-generator = to_device(generator, device)
 
 
 def train_discriminator(real_images, opt_d):
@@ -205,10 +95,6 @@ def train_generator(opt_g):
     return loss.item()
 
 
-sample_dir = "generated"
-os.makedirs(sample_dir, exist_ok=True)
-
-
 def save_sample(index, fixed_latent, show=True):
     fake_images = generator(fixed_latent)
     fake_fname = "generated-images-{0:0=4d}.png".format(index)
@@ -219,14 +105,10 @@ def save_sample(index, fixed_latent, show=True):
         ax.set_xticks([])
         ax.set_yticks([])
         ax.imshow(make_grid(fake_images.cpu().detach()
-                  [:32], nrow=8).permute(1, 2, 0))
+                            [:32], nrow=8).permute(1, 2, 0))
 
 
-fixed_latent = torch.randn(128, latent_size, 1, 1, device=device)
-save_sample(0, fixed_latent, show=True)
-
-
-def fit(epochs, lr_d, lr_g, start_idx=1):
+def fit(train_data, epochs, lr_d, lr_g, start_idx=1):
     torch.cuda.empty_cache()
 
     losses_d = []
@@ -240,7 +122,7 @@ def fit(epochs, lr_d, lr_g, start_idx=1):
                              lr=lr_g, betas=(0.5, 0.999))
 
     for epoch in range(epochs):
-        for real_images, _ in tqdm(train_dl):
+        for real_images, _ in tqdm(train_data):
             loss_d, real_score, fake_score = train_discriminator(
                 real_images, opt_d)
             loss_g = train_generator(opt_g)
@@ -251,15 +133,128 @@ def fit(epochs, lr_d, lr_g, start_idx=1):
         fake_scores.append(fake_score)
 
         print("Epoch: [{}/{}], loss_d: {:.4f}, loss_g: {:.4f}, real_score: {:.4f}, fake_score: {:.4f}".format(
-            epoch+1, epochs, loss_d, loss_g, real_score, fake_score))
+            epoch + 1, epochs, loss_d, loss_g, real_score, fake_score))
 
-        save_sample(epoch+start_idx, fixed_latent, show=False)
+        save_sample(epoch + start_idx, fixed_latent, show=False)
 
     return losses_d, losses_g, real_scores, fake_scores
 
 
-epochs = 2000
-lr_d = 10e-5
-lr_g = 10e-4
+if __name__ == "__main__":
 
-history = [fit(epochs, lr_d, lr_g, start_idx=1)]
+    DATA_DIR = 'E:\\DATA\\Nebulae'
+
+    stats = (.5, .5, .5), (.5, .5, .5)
+
+    transform_ds = T.Compose([
+        T.Resize((128, 128)),
+        T.CenterCrop(128),
+        T.RandomHorizontalFlip(),
+        T.RandomVerticalFlip(),
+        T.ToTensor(),
+        T.Normalize(*stats)
+    ])
+    ds = torchvision.datasets.ImageFolder(root=DATA_DIR, transform=transform_ds)
+
+    batch_size = 16
+
+    train_dl = DataLoader(ds, batch_size, shuffle=True,
+                          num_workers=3, pin_memory=True, drop_last=True)
+
+    show_image(train_dl)
+
+    device = get_device()
+
+    train_dl = DeviceDataLoader(train_dl, device)
+
+    discriminator = nn.Sequential(
+        # in: 128 x 3 x 128 x 128
+
+        nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
+        nn.BatchNorm2d(64),
+        nn.LeakyReLU(0.2, inplace=True),
+        # 128 x 64 x 64 x 64
+
+        nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+        nn.BatchNorm2d(128),
+        nn.LeakyReLU(0.2, inplace=True),
+        # 128 x 128 x 32 x 32
+
+        nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
+        nn.BatchNorm2d(256),
+        nn.LeakyReLU(0.2, inplace=True),
+        # 128 x 256 x 16 x 16
+
+        nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
+        nn.BatchNorm2d(512),
+        nn.LeakyReLU(0.2, inplace=True),
+        # 128 x 512 x 8 x 8
+
+        nn.Conv2d(512, 1024, kernel_size=4, stride=2, padding=1, bias=False),
+        nn.BatchNorm2d(1024),
+        nn.LeakyReLU(0.2, inplace=True),
+        # 128 x 1024 x 4 x 4
+
+        nn.Conv2d(1024, 1, kernel_size=4, stride=1, padding=0, bias=False),
+        # 128 x 1 x 1 x 1
+
+        nn.Flatten(),
+        nn.Sigmoid()
+
+    )
+
+    discriminator = to_device(discriminator, device)
+
+    latent_size = 128
+    generator = nn.Sequential(
+        # in: 128 x 1 x 1
+
+        nn.ConvTranspose2d(latent_size, 1024, kernel_size=4,
+                           stride=1, padding=0, bias=False),
+        nn.BatchNorm2d(1024),
+        nn.ReLU(True),
+        # 128 x 1024 x 4 x 4
+
+        nn.ConvTranspose2d(1024, 512, kernel_size=4,
+                           stride=2, padding=1, bias=False),
+        nn.BatchNorm2d(512),
+        nn.ReLU(True),
+        # 128 x 512 x 8 x 8
+
+        nn.ConvTranspose2d(512, 256, kernel_size=4,
+                           stride=2, padding=1, bias=False),
+        nn.BatchNorm2d(256),
+        nn.ReLU(True),
+        # 128 x 256 x 16 x 16
+
+        nn.ConvTranspose2d(256, 128, kernel_size=4,
+                           stride=2, padding=1, bias=False),
+        nn.BatchNorm2d(128),
+        nn.ReLU(True),
+        # 128 x 128 x 32 x 32
+
+        nn.ConvTranspose2d(128, 64, kernel_size=4,
+                           stride=2, padding=1, bias=False),
+        nn.BatchNorm2d(64),
+        nn.ReLU(True),
+        # 128 x 64 x 64 x 64
+
+        nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
+        # 128 x 3 x 128 x 128
+        nn.Tanh()
+
+    )
+
+    generator = to_device(generator, device)
+
+    sample_dir = "generated"
+    os.makedirs(sample_dir, exist_ok=True)
+
+    fixed_latent = torch.randn(128, latent_size, 1, 1, device=device)
+    save_sample(0, fixed_latent, show=True)
+
+    epochs = 2000
+    lr_d = 10e-5
+    lr_g = 10e-4
+
+    history = [fit(train_dl, epochs, lr_d, lr_g, start_idx=1)]
---
+++ scripts/crop_nebula.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+import os
+
+DIR = 'E:/DATA/Nebulae/train'
+OUT_DIR = 'E:/DATA/Nebulae/train-crop/'
+
+if __name__ == '__main__':
+    invalid_list = []
+    for image_path in os.listdir(DIR):
+
+        is_valid = True
+        img = cv2.imread(DIR + '/' + image_path)
+
+        try:
+            img_height, img_width, channels = img.shape
+        except Exception:
+            invalid_list.append(image_path)
+            is_valid = False
+        if is_valid:
+            # check if the max length is img width or height
+            max_val = max(img_width, img_height)
+            if max_val == img_width:
+                pad_tb = (img_width - img_height) // 2
+                pad_lr = 0
+            else:
+                pad_tb = 0
+                pad_lr = (img_height - img_width) // 2
+
+            cropped_img = cv2.copyMakeBorder(img, pad_tb, pad_tb, pad_lr, pad_lr, cv2.BORDER_REPLICATE)
+
+            # check if we lose a pixel when dividing
+            # and pad again by one pixel
+            img_height, img_width, channels = cropped_img.shape
+            if img_height > img_width:
+                cropped_img = cv2.copyMakeBorder(cropped_img, 0, 0, img_height-img_width, 0, cv2.BORDER_REPLICATE)
+            elif img_width > img_height:
+                cropped_img = cv2.copyMakeBorder(cropped_img, img_width-img_height, 0, 0, 0, cv2.BORDER_REPLICATE)
+
+            # debug visualizer
+            # img_height, img_width, channels = cropped_img.shape
+            # print('Height: ' + str(img_height))
+            # print('Width: ' + str(img_width))
+            # cv2.imshow('cropped', cropped_img)
+            # cv2.waitKey(0)
+
+            # save image to output dir
+            cv2.imwrite(OUT_DIR+image_path, cropped_img)
+
+    print(invalid_list)
