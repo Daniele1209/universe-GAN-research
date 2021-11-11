17ed941d6eb1db7be37803773fe1d749176132e0
Daniele <dany.mosh@yahoo.com>
2021-11-11 19:06:37 +0200

    planet generation on lightweight GAN - epoch 6000

--- scripts/crop_nebula.py
+++ scripts/crop_nebula.py
@@ -1,9 +1,6 @@
-import cv2
-import numpy as np
-import os
 
-DIR = 'D:/DATA/train'
-OUT_DIR = 'D:/DATA/Nebulae/train-crop/'
+DIR = 'E:/DATA/Nebulae/train'
+OUT_DIR = 'E:/DATA/Nebulae/train-crop/'
 
 if __name__ == '__main__':
     invalid_list = []
@@ -11,6 +8,7 @@ if __name__ == '__main__':
 
         is_valid = True
         img = cv2.imread(DIR + '/' + image_path)
+
         try:
             img_height, img_width, channels = img.shape
         except Exception:
@@ -20,28 +18,30 @@ if __name__ == '__main__':
             # check if the max length is img width or height
             max_val = max(img_width, img_height)
             if max_val == img_width:
-                mid_point = img_width // 2
-                cropped_image = img[0:img_height, mid_point-img_height//2:mid_point+img_height//2]
+                pad_tb = (img_width - img_height) // 2
+                pad_lr = 0
             else:
-                mid_point = img_height // 2
-                cropped_image = img[mid_point - img_width // 2:mid_point + img_width // 2, 0:img_width]
+                pad_tb = 0
+                pad_lr = (img_height - img_width) // 2
+
+            cropped_img = cv2.copyMakeBorder(img, pad_tb, pad_tb, pad_lr, pad_lr, cv2.BORDER_REPLICATE)
 
             # check if we lose a pixel when dividing
             # and pad again by one pixel
-            img_height, img_width, channels = cropped_image.shape
+            img_height, img_width, channels = cropped_img.shape
             if img_height > img_width:
-                cropped_image = cv2.copyMakeBorder(cropped_image, 0, 0, img_height-img_width, 0, cv2.BORDER_REPLICATE)
+                cropped_img = cv2.copyMakeBorder(cropped_img, 0, 0, img_height-img_width, 0, cv2.BORDER_REPLICATE)
             elif img_width > img_height:
-                cropped_image = cv2.copyMakeBorder(cropped_image, img_width-img_height, 0, 0, 0, cv2.BORDER_REPLICATE)
+                cropped_img = cv2.copyMakeBorder(cropped_img, img_width-img_height, 0, 0, 0, cv2.BORDER_REPLICATE)
 
             # debug visualizer
-            # img_height, img_width, channels = cropped_image.shape
+            # img_height, img_width, channels = cropped_img.shape
             # print('Height: ' + str(img_height))
             # print('Width: ' + str(img_width))
-            # cv2.imshow('cropped', cropped_image)
+            # cv2.imshow('cropped', cropped_img)
             # cv2.waitKey(0)
 
             # save image to output dir
-            cv2.imwrite(OUT_DIR+image_path, cropped_image)
+            cv2.imwrite(OUT_DIR+image_path, cropped_img)
 
     print(invalid_list)
